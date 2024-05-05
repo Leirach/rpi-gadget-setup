a5e2468be4a847baf61cb32a8fd06451c64ebbf2
Leirach <juandleon27@gmail.com>
2024-05-05 14:01:50 -0600

    removed unnecessary comments, renamed umtprd conf and added endline to it

--- setup-gadget.sh
+++ setup-gadget.sh
@@ -4,23 +4,20 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-apt update
 apt -y install dnsmasq network-manager bridge-utils debian-keyring debian-archive-keyring apt-transport-https umtp-responder
 
 brctl addbr br0
-cp ./etc/dnsmasq.d/usb0 ./etc/dnsmasq.d/usb0 # COPY FILE
+cp ./etc/dnsmasq.d/usb0 /etc/dnsmasq.d/usb0
 
-cp ./etc/network/interfaces.d/usb0 /etc/network/interfaces.d/usb0 # COPY FILE
+cp ./etc/network/interfaces.d/usb0 /etc/network/interfaces.d/usb0
 
-cp ./usr/local/sbin/multigadget /usr/local/sbin/multigadget # COPY FILE
+cp ./usr/local/sbin/multigadget /usr/local/sbin/multigadget
 chmod +x /usr/local/sbin/multigadget
 
-cp ./lib/systemd/system/multigadget.service /lib/systemd/system/multigadget.service # COPY FILE
+cp ./lib/systemd/system/multigadget.service /lib/systemd/system/multigadget.service
 systemctl enable multigadget.service
 
-cp ./etc/umtprd/umtprd.conf-template /etc/umtprd/umtprd.conf-template # COPY FILE
+cp ./etc/umtprd/umtprd.conf-template /etc/umtprd/umtprd.conf-template
 
 echo dtoverlay=dwc2 >> /boot/firmware/config.txt
 
